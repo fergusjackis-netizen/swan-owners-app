013769664b088071171fddf1eb3e61cb45aeeaf9
fergusjackis-netizen <fergusjackis@gmail.com>
2026-04-23 10:10:30 +0100

    Update

--- src/pages/MaintenanceLogs.jsx
+++ src/pages/MaintenanceLogs.jsx
@@ -541,7 +541,6 @@ export default function MaintenanceLogs() {
   const [chatStatus, setChatStatus] = useState('')
   const [chatOpen, setChatOpen] = useState(false)
   const [showDocs, setShowDocs] = useState(false)
-  const [chatImage, setChatImage] = useState(null)
   const [chatImage, setChatImage] = useState(null) // {base64, mediaType, preview}
 
   useEffect(() => { if (user?.uid) loadAssignedYachts() }, [user?.uid])
